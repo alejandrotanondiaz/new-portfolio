2424ee2bde454829a6d05a29ac6aba3576a406a7
Alejandro Tañón Díaz <78818141+alejandrotanondiaz@users.noreply.github.com>
2024-03-05 23:01:56 -0500

    made lab4 changes

---
+++ src/lib/Project.svelte
@@ -0,0 +1,52 @@
+<script>
+export let info = {};
+let p = info;
+export let hLevel = 2;
+</script>
+
+<style>
+  .projects {
+  display: grid;
+  gap: 1em;
+  grid-template-columns: repeat(auto-fill, minmax(18em, 1fr));
+  grid-template-rows: subgrid;
+  article {
+    display: grid;
+    grid-row: span 3;
+    grid-template-rows: subgrid;
+    h2 {
+      min-height: 3em;
+      margin: 0;
+    }
+    
+  }
+}
+
+section {
+  display: flex;
+  flex-direction: column;
+  header{
+    font-weight: bold;
+  }
+  
+  article {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 1em;
+    hgroup {
+      display: flex;
+      /* flex-direction: column; */
+      justify-content: space-between;
+      margin-top: 1px;
+      margin-bottom: 1px;
+    }
+  }
+}
+
+</style>
+
+<article>
+    <svelte:element this={"h" + hLevel}>{ p.title }</svelte:element>
+    <img src="{p.image}" alt="">
+    <p>{p.description}</p>
+</article>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,14 @@
+<script>
+    import projects from '$lib/projects.json';
+    import Project from "$lib/Project.svelte";
+</script>
 <h1>Alejandro J. Tañón Díaz</h1>   
 <img src="images/Stable_Diffusion_Image.png" height="200px"  alt="AI Generated Portrait of Alejandro with a blue suit and white t-shirt">
 <p>Third-Year Undergraduate at MIT majoring in Computer Science and Enginering. Third-Year Undergraduate at MIT majoring in Computer Science and Enginering. Third-Year Undergraduate at MIT majoring in Computer Science and Enginering. Third-Year Undergraduate at MIT majoring in Computer Science and Enginering.</p>
+
+<h2>Latest Projects</h2>
+<div class="projects">
+    {#each projects.slice(0,3) as p}
+        <Project info={p} hLevel=3 />
+    {/each}
+</div>
